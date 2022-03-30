57cae546853782ac7e5e292c0f9299d8e887a455
Feng Wu <tonyffw@hotmail.com>
2022-03-30 20:53:15 +0800

    Update code...

--- src/SubSequenceProblem/NumDistinct.java
+++ src/SubSequenceProblem/NumDistinct.java
@@ -40,4 +40,27 @@ public class NumDistinct {
         }
         return dp[t.length() - 1][s.length() - 1];
     }
+
+    // 另一种动态规划的思路
+    // 状态转移方程:
+    // dp[i][j] = dp[i - 1][j] + (s.charAt(i) == t.charAt(j) ? dp[i - 1][j - 1] : 0)
+    public int numDistinct1(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 || t.length() > s.length()) {
+            return 0;
+        }
+        int[][] dp = new int[s.length()][t.length()];
+        dp[0][0] = s.charAt(0) == t.charAt(0) ? 1 : 0;
+        for (int i = 1; i < s.length(); i++) {
+            dp[i][0] =dp[i - 1][0] + (s.charAt(i) == t.charAt(0) ? 1 : 0);
+        }
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 1; j <= Math.min(i, t.length() -1); j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[s.length() - 1][t.length() - 1];
+    }
 }
